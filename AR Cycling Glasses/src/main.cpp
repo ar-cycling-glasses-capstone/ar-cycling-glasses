080269c7fe1b88ea78e0cbe3e992841729162300
Felix <51464337+felixha00@users.noreply.github.com>
2022-11-28 14:01:19 -0500

    got screen to display hello world, currently trying to get the ky-028 temp sensor working

--- AR Cycling Glasses/src/main.cpp
+++ AR Cycling Glasses/src/main.cpp
@@ -1,9 +1,58 @@
-#include <Arduino.h>
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+int digitalPin = 3; // KY-028 D0
+int analogPin = A0; // KY-028 analog interface
+int digitalVal; // digital readings
+int analogVal; //analog readings
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(digitalPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  // Serial.begin(9600);
+
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Hello, world!");
+  display.display(); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalVal = digitalRead(digitalPin); 
+  if(digitalVal == HIGH) // if temperature threshold reached
+  {
+    digitalWrite(LED_BUILTIN, HIGH); // turn ON Arduino's LED
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW); // turn OFF Arduino's LED
+  }
+  // Read the analog interface
+  analogVal = analogRead(analogPin); 
+  Serial.println(analogVal); // print analog value to serial
+  delay(100);
 }
