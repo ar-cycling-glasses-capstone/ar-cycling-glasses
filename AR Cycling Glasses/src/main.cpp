9e9b5760a2eb7ea1a97b8acb925d7d42dc994a99
Felix <felixha525@gmail.com>
2022-11-28 17:12:30 -0500

    init display to ky-028 temp sensor

--- AR Cycling Glasses/src/main.cpp
+++ AR Cycling Glasses/src/main.cpp
@@ -19,13 +19,13 @@ int digitalVal; // digital readings
 int analogVal; //analog readings
 
 void setup() {
-  pinMode(digitalPin, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  // pinMode(digitalPin, INPUT);
+  // pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(115200);
   // Serial.begin(9600);
 
-
+  analogVal = analogRead(analogPin); 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
@@ -37,22 +37,25 @@ void setup() {
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
   // Display static text
-  display.println("Hello, world!");
+  display.println(analogVal);
   display.display(); 
 }
 
 void loop() {
-  digitalVal = digitalRead(digitalPin); 
-  if(digitalVal == HIGH) // if temperature threshold reached
-  {
-    digitalWrite(LED_BUILTIN, HIGH); // turn ON Arduino's LED
-  }
-  else
-  {
-    digitalWrite(LED_BUILTIN, LOW); // turn OFF Arduino's LED
-  }
-  // Read the analog interface
-  analogVal = analogRead(analogPin); 
-  Serial.println(analogVal); // print analog value to serial
-  delay(100);
+  // digitalVal = digitalRead(digitalPin); 
+  // if(digitalVal == HIGH) // if temperature threshold reached
+  // {
+  //   digitalWrite(LED_BUILTIN, HIGH); // turn ON Arduino's LED
+  // }
+  // else
+  // {
+  //   digitalWrite(LED_BUILTIN, LOW); // turn OFF Arduino's LED
+  // }
+  // // Read the analog interface
+  // analogVal = analogRead(analogPin); 
+  // display.clearDisplay();
+  // display.println(analogVal);
+  // display.display(); 
+  // // // Serial.println(analogVal); // print analog value to serial
+  // delay(200);
 }
